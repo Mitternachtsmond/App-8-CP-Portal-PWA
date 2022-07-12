f428374b2ea6dc906f90dff20f38bfb188ae6f79
Diwakar10091511 <diwakarnew1123@gmail.com>
2022-07-13 00:18:37 +0530

    sorted the data with date

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let montharr=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-
+let array=[];
+let leftContent = document.querySelector(".leftContent");
 const changeDate =(date)=>
 {
     let day=date.getDate();
@@ -24,13 +25,13 @@ const changeDate =(date)=>
 const getDuration=(duration)=>
 {
     let daydiv=24*60*60;
-    let day=Math.round(duration/daydiv);
+    let day=Math.floor(duration/daydiv);
     duration=duration%daydiv;
     daydiv=60*60;
-    let hours=Math.round(duration/daydiv);
+    let hours=Math.floor(duration/daydiv);
     duration=duration%daydiv;
     daydiv=60;
-    let minutes=Math.round(duration/daydiv);
+    let minutes=Math.floor(duration/daydiv);
     duration=duration%daydiv;
     let finalDuration="";
     if(day>0)
@@ -74,107 +75,154 @@ const getHTMLtoAdd=(img,name,insertDate,insertDuration,insertLink)=>
         </div>`;
         return htmlToAdd;
 };
-const fetchFromCFUrl =(url)=>
+
+let cfCheck=false;
+let ccCheck=false;
+let lcCheck=false;
+const fetchFromCFUrl = async (url)=>
 {
-    fetch(url)
-    .then((res)=> res.json())
-    .then((data)=>
-    {
+    try{
+        const res=await fetch(url);
+        const data=await res.json();
+        cfCheck=true;
         let workData=data.result;
         for(var i=0;i<workData.length;i++)
         {
             if(workData[i].phase=="BEFORE"){
-            let leftContent = document.querySelector(".leftContent");
             let element=workData[i];
             let date=new Date(element.startTimeSeconds*1000);
+            let times=element.startTimeSeconds*1000;
+
             let insertDate=changeDate(date);
             let insertDuration=getDuration(element.durationSeconds);
             let img="images/codeforcesIcon.png";
             let insertLink="https://codeforces.com/contests";
-            let htmlToAdd=getHTMLtoAdd(img,element.name,insertDate,insertDuration,insertLink);
-            leftContent.insertAdjacentHTML("afterbegin",htmlToAdd);
+            let obj={
+                image:img,
+                name:element.name,
+                date:insertDate,
+                duration:insertDuration,
+                link:insertLink,
+                dur:element.durationSeconds,
+                time:times
+            };
+            array.push(obj);
             }
             else
             break;
         }
-    })
-    .catch((error)=>
+    }
+    catch
     {
         console.log("error found");
     }
-    );
 };
 
-const fetchFromCCUrl =(url)=>
+const fetchFromCCUrl =async (url)=>
 {
-    fetch(url)
-    .then((res)=> res.json())
-    .then((data)=>
+    try
     {
+        const res=await fetch(url);
+        const data=await res.json();
+        ccCheck=true;
         let workData=data;
         for(var i=0;i<workData.length;i++)
         {
             if(workData[i].status=="BEFORE"){
-            let leftContent = document.querySelector(".leftContent");
             let element=workData[i];
             let date=new Date(element.start_time);
+            let times=date.getTime();
             let insertDate=changeDate(date);
             let insertDuration=getDuration(element.duration);
-
+            let numDur=parseInt(element.duration);
             let img="images/codechefIcon2.png";
             let insertLink=element.url;
-            let htmlToAdd=getHTMLtoAdd(img,element.name,insertDate,insertDuration,insertLink);
-            leftContent.insertAdjacentHTML("afterbegin",htmlToAdd);
+            let obj={
+                image:img,
+                name:element.name,
+                date:insertDate,
+                duration:insertDuration,
+                link:insertLink,
+                dur:numDur,
+                time:times
+            };
+            array.push(obj);
             }
             else
             break;
         }
-    })
-    .catch((error)=>
+    }
+    catch
     {
         console.log("error found");
     }
-    );
 };
 
-const fetchFromLCUrl =(url)=>
+const fetchFromLCUrl = async (url)=>
 {
-    fetch(url)
-    .then((res)=> res.json())
-    .then((data)=>
-    {
+try{
+    const res=await fetch(url);
+    const data=await res.json();
+        lcCheck=true;
         let workData=data;
         for(var i=0;i<workData.length;i++)
         {
             if(workData[i].status=="BEFORE"){
-            let leftContent = document.querySelector(".leftContent");
             let element=workData[i];
             let date=new Date(element.start_time);
+            let times=date.getTime();
             let insertDate=changeDate(date);
             let insertDuration=getDuration(element.duration);
             let img="images/leetcodeIcon.png";
             let insertLink=element.url;
-            let htmlToAdd=getHTMLtoAdd(img,element.name,insertDate,insertDuration,insertLink);
-            leftContent.insertAdjacentHTML("afterbegin",htmlToAdd);
+            let numDur=parseInt(element.duration);
+            let obj={
+                image:img,
+                name:element.name,
+                date:insertDate,
+                duration:insertDuration,
+                link:insertLink,
+                dur:numDur,
+                time:times
+            };
+            array.push(obj);
             }
             else
             break;
         }
-    })
-    .catch((error)=>
-    {
-        console.log("error found");
     }
-    );
-};
-
-
-const fetchDetails=()=>
-{
-    fetchFromCFUrl("https://codeforces.com/api/contest.list?gym=false");
-    fetchFromCCUrl("https://kontests.net/api/v1/code_chef");
-    fetchFromLCUrl("https://kontests.net/api/v1/leet_code");
+catch{
+    console.log("error found");
+}
 }
 
-let body=document.querySelector("body");
-body.addEventListener("load",fetchDetails());
+const fetchDetails=async ()=>
+{
+    await fetchFromCFUrl("https://codeforces.com/api/contest.list?gym=false");
+    await fetchFromCCUrl("https://kontests.net/api/v1/code_chef");
+    await fetchFromLCUrl("https://kontests.net/api/v1/leet_code");
+}
+
+
+async function start(){
+    await fetchDetails();
+
+
+    array.sort(function (a,b){
+        return (a.time - b.time);
+    });
+    for(let i=0;i<array.length;i++)
+    {
+        // console.log(array[i].name);
+        let element=array[i];
+        let htmlToAdd=getHTMLtoAdd(element.image,element.name,element.date,element.duration,element.link);
+        leftContent.insertAdjacentHTML("beforeend",htmlToAdd);
+    };
+
+}
+start();
+setTimeout(()=>{
+if(ccCheck==false && cfCheck==false && lcCheck==false){
+    alert("Check your internet connection");
+}
+},10000);
