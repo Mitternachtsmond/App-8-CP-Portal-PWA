f6b72847c942d4a09edc1d24018109d58c10d08f
Diwakar10091511 <diwakarnew1123@gmail.com>
2022-07-13 01:09:02 +0530

    express initialised for hosting

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const path=require("path");
+const hbs=require("hbs");
+
+const app=express();
+const staticPath=path.join(__dirname,"../public");
+const partialPath=path.join(__dirname,"../templates/partials");
+const viewPath=path.join(__dirname,"../templates/views");
+app.use(express.static(staticPath));
+app.set("view engine","hbs");
+hbs.registerPartials(partialPath);
+app.set("views",viewPath);
+
+app.get("/",(req,res)=>
+{
+    res.render("index");
+});
+app.get("/profile",(req,res)=>
+{
+    res.render("profile");
+});
+app.listen(8000);
+
+
