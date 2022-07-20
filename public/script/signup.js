38d2f629d5a438f84e15287f1155b64c0e568f64
Diwakar10091511 <diwakarnew1123@gmail.com>
2022-07-21 00:19:18 +0530

    minor changes in signup

--- public/script/signup.js
+++ public/script/signup.js
@@ -3,6 +3,7 @@ const login=()=>
     document.location.href="/login";
     console.log("hello i am");
 }
-let loginButton=document.querySelector(".signupContainer");
-loginButton.addEventListener("click",login);
+let loginBtn=document.querySelector(".signupContainer");
+loginBtn.addEventListener("click",login);
 console.log("working");
+
